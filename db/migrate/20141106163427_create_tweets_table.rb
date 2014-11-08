2e6da7b657059f063ae560eea815e4e410e4a11c
Esu Chang <Esu@Esus-M.local>
2014-11-08 19:09:42 +0800

    made some minor changes & completed

--- db/migrate/20141106163427_create_tweets_table.rb
+++ db/migrate/20141106163427_create_tweets_table.rb
@@ -1,7 +1,6 @@
 class CreateTweetsTable < ActiveRecord::Migration
   def change
   	create_table :tweets do |t|
-  		t.integer :twitter_user_id
   		t.text :tweet_text
   		t.string :tweet_id
 
--- db/migrate/20141106163506_create_twitter_users_table.rb
+++ db/migrate/20141106163506_create_twitter_users_table.rb
@@ -2,7 +2,6 @@ class CreateTwitterUsersTable < ActiveRecord::Migration
   def change
   	create_table :twitter_users do |t|
   		t.string :twitter_username
-  		t.integer :twitter_user_id
 
   		t.timestamps
   	end
